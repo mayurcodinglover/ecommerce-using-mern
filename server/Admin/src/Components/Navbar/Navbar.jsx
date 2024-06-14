c737b84f89168caa1d43f31387a60eefc05e4de8
mayur <hedaumayur2003@gmail.com>
2024-06-14 14:38:10 +0530

    new

--- server/Admin/src/Components/Navbar/Navbar.jsx
+++ server/Admin/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import logo from '../../assets/nav-logo.svg'
-import profile from '../../assets/nav-profile.svg'
+import profile from '../../../public/profile.jpg'
 
 const Navbar = () => {
   return (
     <div>
       <div className="navbar flex justify-between items-center mx-2 my-2 m-2 p-1 bg-gray-100">
             <img src={logo} alt="logo-image" />
-            <img src={profile} alt="profile" />
+            <img src={profile} alt="profile"  className='h-[3rem] w-[3rem] rounded-[50%]'/>
       </div>
     </div>
   )
